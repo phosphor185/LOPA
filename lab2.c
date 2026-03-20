8fc3f42fc691c35b9f3389305a518e052ea77ca9
phosphor185 <sunpiexd@gmail.com>
2026-03-20 23:27:13 +1000

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,12 +1,49 @@
 #include <stdio.h>
 
 int main() {
-    int n = 9;
-    scanf("%d", &n);
-    int arr[n]; 
+    double matrix[3][3];
+    int i, j, k;
+    
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%Lf", &matrix[i][j]);
+        }
+ }
+ double sum = 0;
+ 
+ for (i = 0; i < 3; i++) {
+        sum += matrix[i][i];
+    }
+ printf("Summa glav diag: %Lf  ", sum);
+    double sum2 = 0;
+    for (i = 0; i < 3; i++) {
+        sum2 += matrix[i][2 - i];
+ }
+ printf("Summa pob diag: %Lf", sum2);
+ 
+ int res[2][2];
+ int matrix2[2][2];
+ for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%Lf", &matrix2[i][j]);
+        }
+ }
+ for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            res[i][j] = 0;
+            for (k = 0; k < 2; k++) {
+                res[i][j] += matrix2[i][k] * matrix2[k][j];
+            }
+        }
+    }
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", res[i][j]);
+        }
+    }
 
-    printf("Vvedite %Lm elementov:\n", n);
-    for(i = 0, i < n; i++) {
-    	scanf("%Lm", &arr[i]);
-	}
+ 
+    return 0;
 }
